b314fbf2b1ed4c67e773da953b0ce9c548512060
Huy Ngo <46898282+huyngo057@users.noreply.github.com>
2024-04-07 17:47:50 +0700

    submit solution

---
+++ TwoPointers/TwoSumII.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode.TwoPointers;
+
+public class TwoSumII
+{
+	public int[] TwoSum(int[] numbers, int target)
+	{
+		var result = new int[2];
+
+		if (numbers.Length == 2) return new[] { 1, 2 };
+		var i = 0;
+		var j = numbers.Length - 1;
+		while (i < j)
+		{
+			var sum = numbers[i] + numbers[j];
+			if (sum == target)
+			{
+				i++;
+				j++;
+				return new[] { i, j };
+			}
+
+			if (sum > target) j--;
+			else if (sum < target) i++;
+		}
+
+		return result;
+	}
+
+	[Theory]
+	[InlineData(new[] { -1, 0 }, -1)]
+	public void Test(int[] nums, int k)
+	{
+		var check = TwoSum(nums, k);
+		var a = 1;
+	}
+}
