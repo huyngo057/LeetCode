6bfadf47ff6cb22dfbca2c90cc11c0e81a698980
Huy Ngo <46898282+huyngo057@users.noreply.github.com>
2024-04-07 17:18:30 +0700

    submit solution

---
+++ TwoPointers/ValidPalindrome.cs
@@ -0,0 +1,34 @@
+﻿namespace Leetcode.TwoPointers;
+
+public class ValidPalindrome
+{
+	public bool IsPalindrome(string s)
+	{
+		if (s.Length <= 1) return true;
+
+		s = s.ToLower();
+		var i = 0;
+		var j = s.Length - 1;
+		while (i < j)
+		{
+			var left = s[i];
+			var right = s[j];
+
+			if (s[i] != s[j]) return false;
+			i++;
+			j--;
+		}
+
+		return true;
+	}
+
+	[Theory]
+	// [InlineData("A man, a plan, a canal: Panama")]
+	// [InlineData("race a car")]
+	[InlineData("0P")]
+	// [InlineData(" ")]
+	public void Test(string s)
+	{
+		Assert.False(IsPalindrome(s));
+	}
+}
