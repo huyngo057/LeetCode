a576c6e3924abbded0f7b3d09881ea4fd1ceeec3
Huy Ngo <46898282+huyngo057@users.noreply.github.com>
2024-04-29 23:54:43 +0700

    add trap rain water solution

---
+++ TwoPointers/TrappingRainWater.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode.TwoPointers;
+
+public class TrappingRainWater
+{
+	public int Trap(int[] height)
+	{
+		var result = 0;
+		var left = 0;
+		var right = height.Length - 1;
+		var maxLeftHeight = height[left];
+		var maxRightHeight = height[right];
+		while (left < right)
+			if (maxLeftHeight < maxRightHeight)
+			{
+				left++;
+				maxLeftHeight = Math.Max(maxLeftHeight, height[left]);
+				result += maxLeftHeight - height[left];
+			}
+			else
+			{
+				right--;
+				maxRightHeight = Math.Max(maxRightHeight, height[right]);
+				result += maxRightHeight - height[right];
+			}
+
+		return result;
+	}
+
+	[Theory]
+	[InlineData(new[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 })]
+	public void Test(int[] height)
+	{
+		var result = Trap(height);
+		Assert.Equal(6, result);
+	}
+}
