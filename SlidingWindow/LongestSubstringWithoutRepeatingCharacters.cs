b82c453e9491fbde6d67010434c4814c664dc7ec
Huy Ngo <46898282+huyngo057@users.noreply.github.com>
2024-04-30 15:02:32 +0700

    add longest substring solution

---
+++ SlidingWindow/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,33 @@
+﻿namespace Leetcode.SlidingWindow;
+
+public class LongestSubstringWithoutRepeatingCharacters
+{
+	public int LengthOfLongestSubstring(string s)
+	{
+		var left = 0;
+		var result = 0;
+		var charSet = new HashSet<char>();
+
+		foreach (var c in s)
+		{
+			while(charSet.Contains(c))
+			{
+				charSet.Remove(s[left]);
+				left++;
+			}
+
+			charSet.Add(c);
+			result = Math.Max(result, charSet.Count);
+		}
+
+		return result;
+	}
+
+	[Theory]
+	[InlineData("abcabcbb")]
+	public void Test(string s)
+	{
+		var result = LengthOfLongestSubstring(s);
+		Assert.Equal(3, result);
+	}
+}
