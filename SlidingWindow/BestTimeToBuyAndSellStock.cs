8759e5217eeb2304ce42f18bef70f65a7dd57f0a
Huy Ngo <46898282+huyngo057@users.noreply.github.com>
2024-04-30 12:01:01 +0700

    add sliding windown first solution

---
+++ SlidingWindow/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode.SlidingWindow;
+
+public class BestTimeToBuyAndSellStock
+{
+	public int MaxProfit(int[] prices)
+	{
+		var maxProfit = 0;
+		var left = 0;
+		var right = 1;
+
+		while (right < prices.Length)
+		{
+			if (prices[left] < prices[right])
+			{
+				var profit = prices[right] - prices[left];
+				if (maxProfit < profit) maxProfit = profit;
+			}
+			else
+			{
+				left = right;
+			}
+
+			right++;
+		}
+
+		return maxProfit;
+	}
+
+	[Theory]
+	[InlineData(new[] { 1, 2, 4, 2, 5, 7, 2, 4, 9, 0, 9 })]
+	public void Test(int[] prices)
+	{
+		var result = MaxProfit(prices);
+		Assert.Equal(9, result);
+	}
+}
